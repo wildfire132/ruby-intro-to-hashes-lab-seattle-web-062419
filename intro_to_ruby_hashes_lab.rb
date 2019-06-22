ec8bad20daab3233e77d0162503f78c4305de528
Katana Tran <tkatyk21@gmail.com>
2019-06-22 00:02:49 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -15,8 +15,8 @@ end
 
 def monopoly_with_second_tier
 	monopoly = {}
-	monopoly[:railroads] = {}
-
+	monopoly[:railroads] = {:pieces => 4, :names => {}, :rent_in_dollars =>{}}
+  
 end
 
 def monopoly_with_third_tier
